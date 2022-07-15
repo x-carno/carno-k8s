3cc54e8673194f9f83de21a1b02068eab7ab7f56
yan-xi.zhu <547110356@qq.com>
2022-07-15 16:02:04 +0800

    update

--- ver-tauri/carno-k8s/src/settings.js
+++ ver-tauri/carno-k8s/src/settings.js
@@ -1,7 +1,7 @@
 const setting = {
     /*page layout related*/
     //sideBar or navbar show title
-    title: 'Vue3 Admin Plus',
+    title: 'CarnoK8S',
     /**
      * @type {boolean} true | false
      * @description Whether show the logo in sidebar
