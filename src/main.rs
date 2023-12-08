3b1feb2a8054ff0066cd00b33511bb156e214712
zhuyanxi <547110356@qq.com>
2023-12-08 23:21:18 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello"); // s 进入作用域
+
+    takes_ownership(s); // s 的值移动到函数里 ...
+    println!("{}", s); // ... 所以到这里不再有效
+
+    let x = 5; // x 进入作用域
+
+    makes_copy(x); // x 应该移动函数里，
+    println!("{}", x); // 但 i32 是 Copy 的，所以在后面可继续使用 x
 }
+fn takes_ownership(some_string: String) {
+    // some_string 进入作用域
+    println!("{}", some_string);
+} // 这里，some_string 移出作用域并调用 `drop` 方法。占用的内存被释放
+
+fn makes_copy(some_integer: i32) {
+    // some_integer 进入作用域
+    println!("{}", some_integer);
+} // 这里，some_integer 移出作用域。不会有特殊操作
